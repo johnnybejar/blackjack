ed655db431431f31bc300b05c81f8cb7c59d7bae
ThomasSmith11 <94136112+ThomasSmith11@users.noreply.github.com>
2021-11-11 13:24:22 -0500

    Create BlackjackGame.java

---
+++ BlackJack/src/application/BlackjackGame.java
@@ -0,0 +1,84 @@
+import java.util.Scanner;
+
+public class BlackjackGame {
+
+    private Scanner input = new Scanner(System.in);
+    private int users;
+    private Player[] players;
+    private Deck deck;
+    private Dealer dealer = new Dealer();
+
+    public void shuffle() throws InvalidDeckPositionException,
+            InvalidCardSuiteException, InvalidCardValueException {
+        deck.shuffle();
+    }
+
+    public void getBets() {
+        int betValue;
+
+        for (int i = 0; i < users; i++) {
+            if (player[i].getBank() > 0) {
+                do {
+                    System.out.print("How much do you want to bet?");
+                    betValue = input.nextInt();
+                    players[i].setBet(betValue);
+                }
+                while (!(betValue > 0 && betValue <= players[i].getBank()));
+                System.out.println("");
+            }
+        }
+    }
+
+    public void dealCards() {
+        for (int j = 0; j < 2; j++) {
+            for (int i = 0; i < users; i++) {
+                if (players[i].getBank() > 0) {
+                    players[i].addCard(deck.nextCard());
+                }
+            }
+            dealer.addCard(deck.nextCard());
+        }
+    }
+
+    public void checkBlackjack() {
+        if (deal.isBlackjack()) {
+            System.out.println("Dealer has Blackjack!");
+            for (int i = 0; i < users; i++) {
+                if (players[i].getTotal() == 21) {
+                    System.out.println(players[i].getName() + " pushes");
+                    players[i].push();
+                }
+                else {
+                    System.out.println(players[i].getName() + " loses");
+                    players[i].bust();
+                }
+            }
+        }
+        else {
+            if (deal.peek()) {
+                System.out.println("Dealer peeks and does not have a Blackjack");
+            }
+
+            for (int i = 0; i < users; i++) {
+                if (players[i].getTotal() == 21) {
+                    System.out.println(players[i].getName() + " has blackjack!");
+                    players[i].blackjack();
+                }
+            }
+        }
+    }
+
+    public void dealerPlays() {
+        boolean isPlayerInGame = false;
+        for (int i = 0; i < users && isPlayerInGame == false; i++) {
+            if (players[i].getBet() > 0 && players[i].getTotal() <= 21) {
+                isPlayerInGame = true;
+            }
+        }
+        if (isPlayerInGame) {
+            dealer.dealerPlay(deck);
+        }
+    }
+
+
+}
