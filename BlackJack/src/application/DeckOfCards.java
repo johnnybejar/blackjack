5076ec606bff9c3b23267e4cbd51b3fe2b36fe25
johnnyb <88946266+rat-boys@users.noreply.github.com>
2021-11-15 19:23:31 -0500

    Update DeckOfCards.java

--- BlackJack/src/application/DeckOfCards.java
+++ BlackJack/src/application/DeckOfCards.java
@@ -1,40 +1,52 @@
-import java.util.Random;
+package application;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import application.Card.RANKS;
+import application.Card.SUITS;
+
 public class DeckOfCards {
-
-    private Card[] deck;
-
+	private Card[] deck = new Card[52];
+	private int topCardIndex = 51;
+	
     public DeckOfCards(){
-        Card[] newCardDeck = new Card[0];
         int i = 0;
-        for (Card.SUITS s : Card.SUITS.values()){
-            i++;
-            for (Card.RANKS r : Card.RANKS.values()){
-                Card newCard = new Card(s, r);
-                newCardDeck[i] = newCard;
-                deck = newCardDeck;
-            }
-        }
+        for (SUITS suit : SUITS.values()) {
+			for (RANKS rank : RANKS.values()) {
+				deck[i++] = new Card(suit, rank);
+			}
+		}
+        shuffleDeck();
     }
 
-    public void ShuffleDeck(){
-        int randInt;
-        Random rand = new Random();
-        Card temp;
-        for (int i = 0; i < deck.length; i++){
-            randInt = rand.nextInt(deck.length-1);
-            temp = deck[i];
-            deck[i] = deck[randInt];
-            deck[randInt] = temp;
-        }
+    public void shuffleDeck(){
+    	/*
+    	 * Shuffles the deck using the built-in Collections class
+    	 */
+    	Collections.shuffle(Arrays.asList(deck));
+    	
     }
-    //returns Card of the last element in deck and deletes the element by making a new array and assigning it as deck
+    // Returns Card of the last element in deck and changes the element to null
     public Card drawTop(){
-        Card topCard = deck[deck.length-1];
-        Card[] newDeck = new Card[0];
-        for(int i = 0; i < deck.length-1; i++)
-            newDeck[i]=deck[i];
-        deck = newDeck;
-        return topCard;
+    	
+    	Card topCard = deck[topCardIndex];
+    	deck[topCardIndex] = null;
+    	
+    	topCardIndex -= 1;
+    	return topCard;
+    }
+    
+    public Card[] getDeck() {
+    	return this.deck;
+    }
+    
+    @Override
+    public String toString() {
+    	String deckString = "";
+    	for (Card cardTest : deck) {
+    		deckString += cardTest + ", ";
+    	}
+    	return deckString;
     }
 }
-
