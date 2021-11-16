c3f8a51354b991b9f361a239b7d14329f6e3eb5b
johnnyb <88946266+rat-boys@users.noreply.github.com>
2021-11-15 19:22:57 -0500

    Update Card.java

--- BlackJack/src/application/Card.java
+++ BlackJack/src/application/Card.java
@@ -1,21 +1,79 @@
+package application;
+
 public class Card {
     public enum SUITS{
-        CLUBS,
-        DIAMONDS,
-        HEARTS,
-        SPADES;
+        CLUBS("Clubs"),
+        DIAMONDS("Diamonds"),
+        HEARTS("Hearts"),
+        SPADES("Spades");
+    	
+    	String suitValue;
+    	
+    	SUITS(String value) {
+    		this.suitValue = value;
+    	}
+    	
+    	public String getSuitValue() {
+    		return suitValue;
+    	}
     }
     public enum RANKS{
-        TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE;
+    	// We can handle special case for Ace when calculating hand total
+    	// For now, ace is defaulted to 11
+        TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9), TEN(10), JACK(10), QUEEN(10), KING(10), ACE(11);
+    	
+    	int value;
+    	
+    	RANKS(int value) {
+			this.value = value;
+		}
+    	
+    	public int getValue() {
+    		return value;
+    	}
     }
     private SUITS suit;
     private RANKS rank;
+    private int value;
 
     public Card(SUITS suit, RANKS rank){
         this.suit = suit;
         this.rank = rank;
+        this.value = rank.value;
     }
+    
+    public SUITS getSuit() {
+    	return this.suit;
+    }
+    
     public RANKS getRank(){
-        return rank;
+        return this.rank;
+    }
+    
+    public int getValue() {
+    	return this.value;
+    }
+    
+    @Override
+    public String toString() {
+    	String s = String.format("%d of %s", this.rank.getValue(), this.suit.getSuitValue());
+    	switch (this.getRank()) {
+			case ACE:
+				s = String.format("Ace of %s", this.suit.getSuitValue());
+				break;
+			case KING:
+				s = String.format("King of %s", this.suit.getSuitValue());
+				break;
+			case QUEEN:
+				s = String.format("Queen of %s", this.suit.getSuitValue());
+				break;
+			case JACK:
+				s = String.format("Jack of %s", this.suit.getSuitValue());
+				break;
+		default:
+			break;
+    	}
+    	
+    	return s;
     }
 }
