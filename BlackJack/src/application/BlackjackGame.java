9942645f76538ac995ac55bcb7471e73db545386
ThomasSmith11 <94136112+ThomasSmith11@users.noreply.github.com>
2021-11-16 12:31:33 -0500

    Update BlackjackGame.java

--- BlackJack/src/application/BlackjackGame.java
+++ BlackJack/src/application/BlackjackGame.java
@@ -11,80 +11,69 @@ public class BlackjackGame {
     	this.player = player;
     	this.dealer = dealer;
     }
-    	public void newDeck(DeckOfCards newDeck) {
-    		this.deck = newDeck;
-    	}
-    	
-	    public void shuffle() {
-	        deck.ShuffleDeck();
-	    }
-	    
-	    public void deal() {
-	    	this.dealerHand = new Hand(deck);
-	        this.playerHand = new Hand(deck);
-	    }
-	    
-	    public void playerHit() {
-	    	this.playerHand.Draw(deck);
-	    }
-	    
-	    public void playerBet(int bet) {
-	    	player.setBet(bet);
-	    }
-	    
-	    public void doubleBet() {
-	    	int currentBet = player.getBet();
-	    	player.setBet(currentBet*2);
-	    }
-	    
-	    public void allIn() {
-	    	player.setBet(player.getCash());
-	    }
-	    
-	    public boolean checkPlayerBlackjack() {
-	        if (player.blackjack()) {
-	        	return true;
-	        }else return false;
-	    }
-	    
-	    public boolean checkDealerBlackjack() {
-	    	if(dealer.blackjack()) {
-	    		return true;
-	    	}else return false;
-	    }
-	    
-	    public boolean checkPlayerBust() {
-	    	if (player.bust()) {
-	    		return true;
-	    	}else return false;
-	    }
-	    
-	    public boolean checkDealerBust() {
-	    	if (dealer.bust()) {
-	    		return true;
-	    	}else return false;
-	    }
-	    //checks for win.  returns true if player, false if dealer
-	    public boolean checkWinner() {
-	    	if (checkPlayerBlackjack()) {
-	    		return true;
-	    	}
-	    	if(checkDealerBlackjack()) {
-	    		return false;
-	    	}
-	    	if (checkPlayerBust()) {
-	    		return false;
-	    	}
-	    	if (checkDealerBust()) {
-	    		return true;
-	    	}
-	    	if (playerHand.getHandValue() > dealerHand.getHandValue()) {
-	    		return true;
-	    	}
-	    	else return false;
-	    }
-	    
-}
-
-   
+	public void newDeck(DeckOfCards newDeck) {
+		this.deck = newDeck;
+	}
+	
+    public void shuffle() {
+        deck.ShuffleDeck();
+    }
+    
+    public void deal() {
+    	this.dealerHand = new Hand(deck);
+        this.playerHand = new Hand(deck);
+    }
+    
+    public void playerHit() {
+    	this.playerHand.Draw(deck);
+    }
+    
+    public void playerBet(int bet) {
+    	player.setBet(bet);
+    }
+    
+    public void doubleBet() {
+    	int currentBet = player.getBet();
+    	player.setBet(currentBet*2);
+    }
+    
+    public void allIn() {
+    	player.setBet(player.getCash());
+    }
+    
+    public boolean checkPlayerBlackjack() {
+        if (player.blackjack()) {
+        	return true;
+        }else return false;
+    }
+    
+    public boolean checkDealerBlackjack() {
+    	if(dealer.blackjack()) {
+    		return true;
+    	}else return false;
+    }
+    
+    public boolean checkPlayerBust() {
+    	if (player.bust()) {
+    		return true;
+    	}else return false;
+    }
+    
+    public boolean checkDealerBust() {
+    	if (dealer.bust()) {
+    		return true;
+    	}else return false;
+    }
+    
+    public boolean checkFiveCardWinP() {
+    	if (playerHand.getNumCards() == 5) {
+    		return true;
+    	}else return false;
+    }
+    public boolean checkFiveCardWinD() {
+    	if (dealerHand.getNumCards() == 5) {
+    		return true;
+    	}else return false;
+    }
+	   
 }
