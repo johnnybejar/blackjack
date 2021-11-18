71d028260dda3ca913d4c6e663ae6d1f68f8fc66
ThomasSmith11 <94136112+ThomasSmith11@users.noreply.github.com>
2021-11-18 12:42:47 -0500

    Update DeckOfCards.java

--- BlackJack/src/application/DeckOfCards.java
+++ BlackJack/src/application/DeckOfCards.java
@@ -2,53 +2,55 @@ package application;
 
 import java.util.Arrays;
 import java.util.Collections;
+
 import application.Card.RANKS;
 import application.Card.SUITS;
 
 public class DeckOfCards {
-
-    private Card[] deck;
-    private int topCardIndex = 51;
-
+	private Card[] deck = new Card[52];
+	private int topCardIndex = 51;
+	
     public DeckOfCards(){
-        Card[] newCardDeck = new Card[52];
         int i = 0;
         for (SUITS suit : SUITS.values()) {
 			for (RANKS rank : RANKS.values()) {
 				deck[i++] = new Card(suit, rank);
 			}
 		}
-        deck = newCardDeck;
-	// Shuffles the newly created deck as the constructor creates it in order
+     // Shuffles the newly created deck as the constructor creates it in order
         shuffleDeck();
     }
 
     public void shuffleDeck(){
-        /*
+    	/*
     	 * Shuffles the deck using the built-in Collections class
     	 */
     	Collections.shuffle(Arrays.asList(deck));
+    	
     }
-    
+
     public Card drawTop(){
-	/*
-	Grabs the card at the top of the deck or the last card in the array,
-	sets the index to null and returns the card
-	*/
-        Card topCard = deck[topCardIndex];
-        deck[topCardIndex] = null;
-        
-        topCardIndex -= 1;
-        return topCard;
+    	/*
+    	Grabs the card at the top of the deck or the last card in the array,
+    	sets the index to null and returns the card
+    	*/
+    	Card topCard = deck[topCardIndex];
+    	deck[topCardIndex] = null;
+    	
+    	topCardIndex -= 1;
+    	return topCard;
     }
     
-  //for testing purposes inside ide
+    public Card[] getDeck() {
+    	return this.deck;
+    }
+    
     @Override
     public String toString() {
-    	String hold = "";
-    	for (int i = 0; i < deck.length; i++) {
-    		hold += (deck[i].toString() + "; ");
+    	String deckString = "";
+    	for (Card cardTest : deck) {
+    		deckString += cardTest + ", ";
     	}
-    	return hold;
+    	return deckString;
     }
 }
