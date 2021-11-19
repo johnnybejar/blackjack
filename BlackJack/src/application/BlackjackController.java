1e9f02a45df4370730b65d6e727c6f084c6731fa
ThomasSmith11 <94136112+ThomasSmith11@users.noreply.github.com>
2021-11-18 19:43:58 -0500

    Update BlackjackController.java

--- BlackJack/src/application/BlackjackController.java
+++ BlackJack/src/application/BlackjackController.java
@@ -1,8 +1,6 @@
 package application;
 
 import javafx.application.Platform;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -18,12 +16,6 @@ public class BlackjackController {
 	private BlackjackGame game;
 	
 	private int playerBet = 50;
-	
-	private boolean startGame = false;
-    
-    private boolean continueGame = true;
-    
-    private boolean playerStand = false;
 
 	@FXML
 	Button dealButton;
@@ -98,14 +90,63 @@ public class BlackjackController {
 		doubleButton.setDisable(true);
 		minusBet.setDisable(true);
 	}
+	public void peekDealer() {
+		dealerCard1.setText(dealer.getHand()[0].toString());
+		dealerTotal.setText(Integer.toString(dealer.getHandValue()));
+	}
+	
+	public void playerLoses() {
+		player.removeCash(playerBet);
+		bankTotal.setText(Integer.toString(player.getCash()));
+		if (player.getCash() == 0) {
+			
+			gameOutcome.setText("Game Over");
+			hitButton.setDisable(true);
+			standButton.setDisable(true);
+			doubleButton.setDisable(true);
+			minusBet.setDisable(true);
+			dealButton.setDisable(true);
+			addBet.setDisable(true);
+			minusBet.setDisable(true);
+			
+		}else {
+			gameOutcome.setText("Dealer Wins!");
+			resetDefaults();
+		}
+	}
+	
+	public void playerWins() {
+		player.addCash(playerBet);
+		bankTotal.setText(Integer.toString(player.getCash()));
+		gameOutcome.setText("Player Wins!");
+		resetDefaults();
+	}
+
+	public void resetDefaults() {
+		hitButton.setDisable(true);
+		standButton.setDisable(true);
+		doubleButton.setDisable(true);
+		minusBet.setDisable(true);
+		dealButton.setDisable(false);
+		addBet.setDisable(false);
+		minusBet.setDisable(false);
+		playerBet = 50;
+		betTotal.setText("50");
+	}
+	
 	
 	public void findWinner() {
 		if (player.getHandValue() > dealer.getHandValue()) {
-			System.out.println("Player wins!");
+
+			playerWins();
+			
 		} else if (player.getHandValue() < dealer.getHandValue()) {
-			System.out.println("Dealer wins...");
+			
+			playerLoses();
+			
 		} else {
-			System.out.println("Draw!");
+			gameOutcome.setText("Draw...");
+			resetDefaults();
 		}
 	}
 	
@@ -114,9 +155,27 @@ public class BlackjackController {
 		Platform.exit();
 	}
 	
-	//initializes a new deck, shuffles it, then deals to a player and dealer,
+	//first clears all text from previous hand, then initializes a new deck, shuffles it, then deals to a player and dealer,
 	//then displays cards and total value
 	public void dealButtonPressed(ActionEvent e) {
+
+		playerCard1.setText("");
+		playerCard2.setText("");
+		playerCard3.setText("");
+		playerCard4.setText("");
+		playerCard5.setText("");
+		
+		dealerCard1.setText("");
+		dealerCard2.setText("");
+		dealerCard3.setText("");
+		dealerCard4.setText("");
+		dealerCard5.setText("");
+
+		playerTotal.setText("0");
+		dealerTotal.setText("0");
+		
+		gameOutcome.setText("");
+		
 		DeckOfCards deck = new DeckOfCards();
 		BlackjackGame game = new BlackjackGame(deck, player, dealer);
 		this.game = game;
@@ -130,18 +189,18 @@ public class BlackjackController {
 			playerCard1.setText(player.getHand()[0].toString());
 			playerCard2.setText(player.getHand()[1].toString());
 			
-			dealerCard1.setText(dealer.getHand()[0].toString());
+			dealerCard1.setText("?????????????");
 			dealerCard2.setText(dealer.getHand()[1].toString());
 			
 			playerTotal.setText(Integer.toString(player.getHandValue()));
-			dealerTotal.setText(Integer.toString(dealer.getHandValue()));
+			dealerTotal.setText(Integer.toString(dealer.getHand()[1].getValue()));
 			
 		} catch (Exception e1) {
 			e1.printStackTrace();
 		}
 		dealButton.setDisable(true);
-		// addBet.setDisable(true);
-		// minusBet.setDisable(true);
+		addBet.setDisable(true);
+		minusBet.setDisable(true);
 		hitButton.setDisable(false);
 		standButton.setDisable(false);
 		doubleButton.setDisable(false);
@@ -182,29 +241,12 @@ public class BlackjackController {
 				return;
 			}else {
 				hitButton.setDisable(true);
-				game.dealerPlays();
-				int j = 0;
-				while (dealer.getHand()[j] != null) {
-					j++;
-					if (j == 5) {
-						return;
-					}
-					switch(j-1) {
-					case 2:
-						dealerCard3.setText(dealer.getHand()[2].toString());
-						dealerTotal.setText(Integer.toString(dealer.getHandValue()));
-						break;
-					case 3:
-						dealerCard4.setText(dealer.getHand()[3].toString());
-						dealerTotal.setText(Integer.toString(dealer.getHandValue()));
-						break;
-					case 4:
-						dealerCard5.setText(dealer.getHand()[4].toString());
-						dealerTotal.setText(Integer.toString(dealer.getHandValue()));
-						break;
-					default:
-						break;
-					}
+				if (game.checkPlayerBlackjack() || game.playerCount5()) {
+					playerWins();
+				}
+				
+				if (game.checkPlayerBust()) {
+					playerLoses();	
 				}
 				
 				
@@ -219,9 +261,9 @@ public class BlackjackController {
 		If this button is pressed, the player cannot make anymore decisions until
 		the final result of the game is decided
 		*/
-		playerStand = true;
 		hitButton.setDisable(true);
 		standButton.setDisable(true);
+		peekDealer();
 		game.dealerPlays();
 		int j = 0;
 		while (dealer.getHand()[j] != null) {
@@ -246,14 +288,24 @@ public class BlackjackController {
 				break;
 			}
 		}
-		
+		if (game.checkDealerBlackjack() || game.checkDealerBust() || game.dealerCount5()) {
+			if (game.checkDealerBlackjack() || game.dealerCount5()) {
+				playerLoses();
+			}
+			
+			if (game.checkDealerBust()) {
+				playerWins();
+			}
+		}else findWinner();
 		
 	}
 	
 	public void doubleButtonPressed(ActionEvent e) {
-		int currentBet = player.getBet();
-    	player.setBet(currentBet*2);
-    	
+    	if (playerBet*2 <= player.getCash()) {
+    		playerBet *= 2;
+			betTotal.setText(Integer.toString(playerBet));
+			doubleButton.setDisable(true);
+    	}else doubleButton.setDisable(true);
 	}
 	
 	public void addButtonPressed(ActionEvent e) {
