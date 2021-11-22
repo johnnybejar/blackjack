1f1fc973ef0b1c0624c0e6e84cc5e33d94f511dc
johnnyb <88946266+rat-boys@users.noreply.github.com>
2021-11-21 20:10:17 -0500

    Update Blackjack.java

--- BlackJack/src/application/Blackjack.java
+++ BlackJack/src/application/Blackjack.java
@@ -1,4 +1,20 @@
 package application;
+
+/*
+ * Names: John Bejar, Daisy Fanjoy, Thomas Smith, Ben Williams
+ * Class: CSC 331-003
+ * Professor: Professor White
+ * 
+ * Project Description: Blackjack
+ * - Blackjack game created using Java and JavaFX
+ * - Allows the player to play continuously until
+ * 	 they run out of money or they give up
+ * - Dealer must draw to 16 and stand on 17
+ * - Uses the 5 card rule meaning that if a player
+ *   has 5 cards in their hand and does not bust,
+ *   it's considered a win
+ * 
+ */
 	
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
